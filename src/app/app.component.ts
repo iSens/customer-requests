14bab441218eb0fc99d1063d2911c70cb095a025
DK <denis@kishinevsky.ca>
2019-10-20 18:27:02 -0400

    CR read mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  [x: string]: any;
+  date: string;
+  customerName: string;
+  customerPhone: string;
+  brand: string;
+  notes: string;
+  status: string;
+  deposit: string;
   title = 'Customer Requests';
 
-  constructor(private http: Http) {
-    http.get('https://kkw-customer-requests.herokuapp.com/customer_requests.json')
-      .subscribe(res => this.customer_requests = res.json());
+  clients = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+
+    this.dataService.sendGetRequest().subscribe((data: any[])=>{
+      this.clients = data;
+      console.log(this.clients);
+    })      
   }
+
+  displayedColumns: string[] = ['date', 'customerName', 'customerPhone', 'brand', 'notes', 'status', 'deposit'];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
@@ -11,9 +13,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     HttpModule,
+    HttpClientModule,
+    MatTableModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
